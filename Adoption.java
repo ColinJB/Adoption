aecfe71a21371c0cf1f74dff8d4df2fbd0da01ec
Colin Bloom and Gloria Friesen <colin@>
2017-03-13 09:32:38 -0700

    initial commit, create Animal class

---
+++ Adoption.java
@@ -0,0 +1,11 @@
+public class Adoption {
+  public static void main(String[] args) {
+
+    class Animal {
+      public String mSpecies;
+      public String mSex;
+      public int mAge;
+      public String mColor;
+    }
+  }
+}
